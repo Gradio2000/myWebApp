14dcf99b136eb269a4e8acd7c309503509bf1269
Gradio2000 <laskin999@gmail.com>
2021-03-23 18:39:27 +0500

    Initial commit

---
+++ src/main/java/ru/laskin/myWebApp/controllers/MainController.java
@@ -0,0 +1,13 @@
+package ru.laskin.myWebApp.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/main")
+    public String getStart(){
+        return "index";
+    }
+}
