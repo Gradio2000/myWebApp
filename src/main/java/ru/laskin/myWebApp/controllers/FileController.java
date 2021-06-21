a8deb4e53e31312aa4c262612e1da15f260dc39a
Gradio2000 <laskin999@gmail.com>
2021-06-21 16:27:58 +0500

    96

--- src/main/java/ru/laskin/myWebApp/controllers/FileController.java
+++ src/main/java/ru/laskin/myWebApp/controllers/FileController.java
@@ -4,7 +4,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 import ru.laskin.myWebApp.model.Answer;
 import ru.laskin.myWebApp.model.Question;
