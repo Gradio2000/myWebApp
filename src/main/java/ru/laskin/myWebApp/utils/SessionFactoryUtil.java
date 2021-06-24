f0dc9fdfa30947dc56554b606a0a9b0ac1281485
Gradio2000 <laskin999@gmail.com>
2021-06-24 22:45:41 +0500

    104

--- src/main/java/ru/laskin/myWebApp/utils/SessionFactoryUtil.java
+++ src/main/java/ru/laskin/myWebApp/utils/SessionFactoryUtil.java
@@ -1,13 +1,10 @@
 package ru.laskin.myWebApp.utils;
 
 import org.hibernate.HibernateException;
-import org.hibernate.Metamodel;
-import org.hibernate.query.Query;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
-import javax.persistence.metamodel.EntityType;
 
 
 public class SessionFactoryUtil {
@@ -17,7 +14,6 @@ public class SessionFactoryUtil {
         try {
             Configuration configuration = new Configuration();
             configuration.configure();
-
             ourSessionFactory = configuration.buildSessionFactory();
         } catch (Throwable ex) {
             throw new ExceptionInInitializerError(ex);
